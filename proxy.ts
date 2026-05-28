27ec72888689703800a4b02ed2a5eeb395b58646
OilStone Inc <adjabengvincent@gmail.com>
2026-05-28 11:57:17 -0700

    mater

--- proxy.ts
+++ proxy.ts
@@ -6,6 +6,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/webhooks(.*)",
+  "/unauthorized(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
